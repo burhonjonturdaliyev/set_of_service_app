2142fb978cae093892be2fd8c762cdf1a0f811ee
Burkhonjon Turdialiev <burhonjonturdaliyev2003@gmail.com>
2023-03-14 16:18:28 +0500

    Personal setting vs editing page updates

---
+++ lib/Widgets/profile_settings_widgets.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/intl.dart';
+
+// ignore: camel_case_types
+class Profile_setting_widgets extends StatefulWidget {
+  TextEditingController ism;
+  TextEditingController familya;
+  TextEditingController nomer;
+  TextEditingController sana;
+  TextEditingController davlat;
+  Profile_setting_widgets(
+      {super.key,
+      required this.ism,
+      required this.familya,
+      required this.nomer,
+      required this.sana,
+      required this.davlat});
+
+  @override
+  State<Profile_setting_widgets> createState() =>
+      _Profile_setting_widgetsState();
+}
+
+class _Profile_setting_widgetsState extends State<Profile_setting_widgets> {
+  // TextEditingController sana = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: TextFormField(
+                controller: widget.ism,
+                decoration: InputDecoration(
+                  labelText: "Ism",
+
+                  //helperText: "",
+                  hintText: "Ism",
+                  hintStyle: TextStyle(
+                      color: Colors.black26,
+                      fontSize: 16.sp,
+                      fontFamily: "Inter"),
+                  //border: InputBorder.none
+                ),
+              ),
+            )
+          ],
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: TextFormField(
+                controller: widget.familya,
+                decoration: InputDecoration(
+                  labelText: "Familya",
+
+                  // helperText: "",
+                  hintText: "Familya",
+                  hintStyle: TextStyle(
+                      color: Colors.black26,
+                      fontSize: 16.sp,
+                      fontFamily: "Inter"),
+                  //border: InputBorder.none
+                ),
+              ),
+            )
+          ],
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: TextFormField(
+                controller: widget.nomer,
+                decoration: InputDecoration(
+                  labelText: "Telefon nomer",
+                  //helperText: "Telefon nomer",
+                  hintText: "Telefon nomer",
+                  hintStyle: TextStyle(
+                      color: Colors.black26,
+                      fontSize: 16.sp,
+                      fontFamily: "Inter"),
+                  //border: InputBorder.none
+                ),
+              ),
+            )
+          ],
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: TextFormField(
+                controller: widget.sana,
+                onTap: () async {
+                  DateTime? choose = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(1900),
+                      lastDate: DateTime.now());
+                  if (choose != null) {
+                    setState(() {
+                      widget.sana.text =
+                          DateFormat("yyyy-MMMM-dd").format(choose);
+                    });
+                  }
+                },
+                decoration: InputDecoration(
+                  labelText: "Sanani tanlang",
+                  //helperText: "Tug'ilgan sana",
+                  hintText: "Tug'ilgan sana",
+                  hintStyle: TextStyle(
+                      color: Colors.black26,
+                      fontSize: 16.sp,
+                      fontFamily: "Inter"),
+                  //border: InputBorder.none
+                ),
+              ),
+            )
+          ],
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: TextFormField(
+                controller: widget.davlat,
+                decoration: InputDecoration(
+                  //  helperText: "Qaysi davlat servislar",
+                  hintText: "Qaysi davlat servislar",
+                  hintStyle: TextStyle(
+                      color: Colors.black26,
+                      fontSize: 16.sp,
+                      fontFamily: "Inter"),
+
+                  //border: InputBorder.none
+                ),
+              ),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
--- lib/pages/Profile_settings/Profile_settings.dart
+++ lib/pages/Profile_settings/Profile_settings.dart
@@ -1,11 +1,36 @@
 // ignore: file_names
 // ignore_for_file: file_names, duplicate_ignore
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:set_of_service_app/Widgets/profile_settings_widgets.dart';
 
-class Profile_settings extends StatelessWidget {
+class Profile_settings extends StatefulWidget {
   const Profile_settings({super.key});
+
+  @override
+  State<Profile_settings> createState() => _Profile_settingsState();
+}
+
+class _Profile_settingsState extends State<Profile_settings> {
+  TextEditingController ism = TextEditingController();
+  TextEditingController familya = TextEditingController();
+  TextEditingController nomer = TextEditingController();
+  TextEditingController sana = TextEditingController();
+  TextEditingController davlat = TextEditingController();
+  final ImagePicker _picker = ImagePicker();
+  File? _imageFile;
+
+  Future<void> _pickImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _imageFile = File(pickedFile!.path);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,10 +67,47 @@ class Profile_settings extends StatelessWidget {
                         ),
                       ),
                     ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Expanded(
+                      child: Profile_setting_widgets(
+                          ism: ism,
+                          familya: familya,
+                          nomer: nomer,
+                          sana: sana,
+                          davlat: davlat),
+                    ),
                   )
                 ],
               ),
-            )
+            ),
+            Positioned(
+                bottom: 40.h,
+                left: 48.w,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    width: 281.w,
+                    height: 46.h,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        color: const Color(0xff8B0000)),
+                    child: Center(
+                      child: Text(
+                        "Saqlash",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.w700,
+                            fontFamily: "Inter",
+                            letterSpacing: 1.5),
+                      ),
+                    ),
+                  ),
+                ))
           ],
         ),
       ),
