3eb88422af300e6f46b5308098ea7d6fd570d77d
Burkhonjon Turdialiev <burhonjonturdaliyev2003@gmail.com>
2023-04-08 15:13:20 +0500

    New updates

--- lib/pages/Navigation_screens/services_page/yangiliklar/new_with_comment.dart
+++ lib/pages/Navigation_screens/services_page/yangiliklar/new_with_comment.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_import
+
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -19,79 +21,103 @@ class News_comments extends StatefulWidget {
 
 class _News_commentsState extends State<News_comments> {
   TextEditingController send_message = TextEditingController();
-  List<comment_models> comment = [];
-  Future<void> _fetchComment() async {
-    try {
-      debugPrint("Yuklash boshlandi");
-      const uri = "https://randomuser.me/api/?results=25";
-      // final uri = widget.yangilik.list_url;
-      final url = Uri.parse(uri);
-      final response = await http.get(url);
-      final body = response.body;
-      final json = jsonDecode(body);
-      final transforming = json["results"] as List<dynamic>;
-      final result = transforming.map((e) {
-        final name = e["name"]["first"];
-        return comment_models(
-            name: name,
-            message: e["country"],
-            check_id: false,
-            time: e["dob"]["date"]);
-      }).toList();
+  List<comment_models> comment = [
+    comment_models(
+        check_id: false,
+        message: "Salom",
+        name: "Avaz",
+        time: DateTime(2023, 7, 7, 7, 7, 7))
+  ];
+  // Future<void> _fetchComment() async {
+  //   try {
+  //     debugPrint("Yuklash boshlandi");
+  //     final uri = widget.yangilik.list_url;
+  //     // final uri = widget.yangilik.list_url;
+  //     final url = Uri.parse(uri);
+  //     final response = await http.get(url);
+  //     final body = response.body;
+  //     final json = jsonDecode(body ?? "");
+  //     final transforming = json["results"] as List<dynamic>;
+  //     final result = transforming.map((e) {
+  //       final name = e["name"]["first"];
+  //       return comment_models(
+  //           name: name,
+  //           message: e["country"],
+  //           check_id: false,
+  //           time: e["dob"]["date"]);
+  //     }).toList();
 
-      setState(() {
-        comment = result;
-      });
+  //     setState(() {
+  //       comment = result;
+  //     });
 
-      debugPrint("Yuklash tugatildi");
-    } catch (e) {
-      print("XAto $e");
-    }
-  }
+  //     debugPrint("Yuklash tugatildi");
+  //   } catch (e) {
+  //     print("XAto $e");
+  //   }
+  // }
 
   _addingElement() {
-    comment.add(comment_models(
-        name: "Burkhonjon",
-        message: send_message.text,
-        check_id: true,
-        time: DateTime.now()));
+    setState(() {
+      comment.add(comment_models(
+          name: "Burkhonjon",
+          message: send_message.text,
+          check_id: true,
+          time: DateTime.now()));
+    });
+    send_message.clear();
   }
 
   @override
   void initState() {
-    _fetchComment();
+    // _fetchComment();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: comment.isEmpty ? Colors.white : const Color(0xFF8B0000),
-      body: comment.isEmpty
-          ? const Center(child: CircularProgressIndicator())
-          : SafeArea(
-              child: Container(
-                width: 375.w,
-                height: 812.h,
-                decoration: const BoxDecoration(
-                    color: Colors.white,
-                    image: DecorationImage(
-                        image: AssetImage("image/back_screen.png"),
-                        fit: BoxFit.cover)),
-                child: Column(children: [
-                  _image_provider(widget.yangilik),
-                  SizedBox(
-                    height: 10.h,
-                  ),
-                  Padding(
-                      padding:
-                          EdgeInsets.only(left: 5.w, right: 5.w, bottom: 10.h),
-                      child: _info_provider(widget.yangilik)),
-                  Expanded(child: _comment_provider()),
-                  send_message_provider()
-                ]),
-              ),
+      appBar: AppBar(
+          backgroundColor: const Color(0xFF8B0000),
+          centerTitle: true,
+          title: Text(
+            "Komment",
+            style: TextStyle(
+                fontSize: 18.sp,
+                fontFamily: "Inter",
+                fontWeight: FontWeight.bold,
+                letterSpacing: 5.w),
+          )),
+      backgroundColor:
+          // comment.isEmpty ? Colors.white :
+          const Color(0xFF8B0000),
+      body:
+          // comment.isEmpty
+          //     ? const Center(child: CircularProgressIndicator())
+          //     :
+          SafeArea(
+        child: Container(
+          width: 375.w,
+          height: 812.h,
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              image: DecorationImage(
+                image: AssetImage("image/back_screen.png"),
+                fit: BoxFit.fill,
+              )),
+          child: Column(children: [
+            _image_provider(widget.yangilik),
+            SizedBox(
+              height: 10.h,
             ),
+            Padding(
+                padding: EdgeInsets.only(left: 5.w, right: 5.w, bottom: 10.h),
+                child: _info_provider(widget.yangilik)),
+            Expanded(child: _comment_provider()),
+            send_message_provider()
+          ]),
+        ),
+      ),
     );
   }
 
@@ -123,12 +149,78 @@ class _News_commentsState extends State<News_comments> {
       height: 300.h,
       width: double.infinity,
       decoration: const BoxDecoration(color: Color.fromARGB(22, 244, 67, 54)),
-      child: ListView.builder(
-        itemCount: comment.length,
-        itemBuilder: (context, index) {
-          return Container();
-        },
+      child: Padding(
+        padding: EdgeInsets.only(top: 5.h),
+        child: ListView.builder(
+          itemCount: comment.length,
+          itemBuilder: (context, index) {
+            return get_comments(comment[index]);
+          },
+        ),
       ),
+    );
+  }
+
+  get_comments(comment_models comment) {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 5.w, right: 5.w),
+          child: Row(
+            mainAxisAlignment: comment.check_id
+                ? MainAxisAlignment.end
+                : MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Visibility(
+                visible: comment.check_id ? false : true,
+                child: CircleAvatar(
+                  radius: 15.w,
+                  child: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                    size: 25.w,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 5.w,
+              ),
+              Container(
+                constraints: BoxConstraints(maxWidth: 250.w),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(21.w),
+                    color: Colors.blueAccent),
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Text(
+                    comment.message,
+                    style: TextStyle(
+                        fontFamily: "Inter",
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 20.sp),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 5.w,
+              ),
+              Visibility(
+                visible: comment.check_id,
+                child: CircleAvatar(
+                  radius: 15.w,
+                  child: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                    size: 25.w,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 
@@ -137,7 +229,7 @@ class _News_commentsState extends State<News_comments> {
       width: double.infinity,
       height: 50.h,
       decoration: BoxDecoration(
-          color: Color.fromARGB(255, 191, 191, 191),
+          color: const Color.fromARGB(255, 191, 191, 191),
           borderRadius: BorderRadius.circular(21.w)),
       child: Padding(
         padding: EdgeInsets.only(left: 10.w, right: 5.w),
@@ -161,11 +253,7 @@ class _News_commentsState extends State<News_comments> {
             ),
           ),
           GestureDetector(
-            onTap: () {
-              setState(() {
-                send_message.clear();
-              });
-            },
+            onTap: _addingElement,
             child: CircleAvatar(
               backgroundColor: Colors.white,
               child: Center(
