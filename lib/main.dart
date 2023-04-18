d0e6d4331973a4997847afd4825cbfafafcf8325
Burkhonjon Turdialiev <burhonjonturdaliyev2003@gmail.com>
2023-04-18 21:06:21 +0500

    Last updates with airtickets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:set_of_service_app/Splash_screen/splash_screen.dart';
-import 'package:set_of_service_app/pages/Home/airticket/page/air_ticket_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,9 +18,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.red,
         ),
-
-        home: Air_ticket_list(),
-        // home: const Splash_screen(),
+        home: const Splash_screen(),
       ),
       designSize: const Size(375, 812),
     );
--- lib/pages/Home/airticket/page/widget/ticket_builder.dart
+++ lib/pages/Home/airticket/page/widget/ticket_builder.dart
@@ -6,20 +6,12 @@ import 'package:intl/intl.dart';
 
 import 'package:set_of_service_app/pages/Home/airticket/page/models/Api_models.dart';
 
-import '../../../../../models/colour_model.dart';
-
 class Ticket_builder extends StatelessWidget {
   Ticket_builder({super.key, required this.models});
   List<Api_models> models;
 
-  List<Colour_models> colour = [
-    Colour_models(
-        rang2: const Color.fromARGB(204, 255, 0, 0), rang1: Colors.white),
-  ];
-
   @override
   Widget build(BuildContext context) {
-    int size = colour.length;
     return Container(
       height: double.infinity,
       width: double.infinity,
@@ -43,170 +35,172 @@ class Ticket_builder extends StatelessWidget {
   }
 }
 
-Widget pochta_items(
-    BuildContext context, Colour_models colourModels, Api_models api) {
-  dialog() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          backgroundColor: const Color(0xffFDDADA),
-          content: SizedBox(
-            height: 320.h,
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              RichText(
-                  text: TextSpan(children: [
-                TextSpan(
-                  text: "Qayerdan: ",
-                  style: TextStyle(
-                      color: Colors.black54,
-                      fontFamily: "Inter",
-                      fontSize: 16.sp,
-                      fontWeight: FontWeight.w600),
-                ),
-                TextSpan(
-                  text: api.fromTo!,
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: "Inter",
-                      fontSize: 18.sp,
-                      fontWeight: FontWeight.w600),
-                )
-              ])),
-            ]),
-          ),
-        );
-      },
-    );
-  }
+// Widget pochta_items(
+//     BuildContext context, Colour_models colourModels, Api_models api) {
+//   dialog() {
+//     showDialog(
+//       context: context,
+//       builder: (context) {
+//         return AlertDialog(
+//           backgroundColor: const Color(0xffFDDADA),
+//           content: SizedBox(
+//             height: 320.h,
+//             child:
+//                 Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+//               RichText(
+//                   text: TextSpan(children: [
+//                 TextSpan(
+//                   text: "Qayerdan: ",
+//                   style: TextStyle(
+//                       color: Colors.black54,
+//                       fontFamily: "Inter",
+//                       fontSize: 16.sp,
+//                       fontWeight: FontWeight.w600),
+//                 ),
+//                 TextSpan(
+//                   text: api.fromTo!,
+//                   style: TextStyle(
+//                       color: Colors.black,
+//                       fontFamily: "Inter",
+//                       fontSize: 18.sp,
+//                       fontWeight: FontWeight.w600),
+//                 )
+//               ])),
+//             ]),
+//           ),
+//         );
+//       },
+//     );
+//   }
 
-  return Padding(
-    padding: EdgeInsets.only(top: 5.h, bottom: 5.0.h, left: 5.w, right: 5.w),
-    child: InkWell(
-      onTap: dialog,
-      child: Container(
-        width: 353.w,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(33.w),
-          border: Border.all(width: 1, color: Colors.black26),
-          gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [colourModels.rang1, colourModels.rang2]),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Text(
-                    api.createdBy == null ? "Air ticket" : "${api.createdBy}",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontFamily: "Inter",
-                        fontWeight: FontWeight.w400,
-                        fontSize: 10.sp),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 5.h,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  RichText(
-                      text: TextSpan(children: [
-                    TextSpan(
-                      text: "Qayerga: ",
-                      style: TextStyle(
-                          color: Colors.black54,
-                          fontSize: 18.sp,
-                          fontFamily: "Inter",
-                          fontWeight: FontWeight.w500),
-                    ),
-                    TextSpan(
-                      text: api.whereTo,
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 24.sp,
-                          fontFamily: "Inter",
-                          fontWeight: FontWeight.w800),
-                    )
-                  ])),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  RichText(
-                      text: TextSpan(children: [
-                    TextSpan(
-                      text: "Qayerdan: ",
-                      style: TextStyle(
-                          color: Colors.black54,
-                          fontSize: 18.sp,
-                          fontFamily: "Inter",
-                          fontWeight: FontWeight.w500),
-                    ),
-                    TextSpan(
-                      text: api.fromTo,
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 24.sp,
-                          fontFamily: "Inter",
-                          fontWeight: FontWeight.w800),
-                    )
-                  ])),
-                ],
-              ),
-              SizedBox(
-                height: 10.h,
-              ),
-              Row(
-                children: [Text("Parvoz turi:"), Text(api.airClass!)],
-              ),
-              SizedBox(
-                height: 10.h,
-              ),
-              Row(
-                children: [
-                  Text("Borish kuni: "),
-                  Text(DateFormat("dd.MM.yyyy").format(api.toGoDate)),
-                  SizedBox(
-                    width: 10.w,
-                  ),
-                  Text("Qaytish kuni: "),
-                  Text(DateFormat("dd.MM.yyyy").format(api.returnDate))
-                ],
-              ),
-              SizedBox(
-                height: 10.h,
-              ),
-              SizedBox(
-                height: 5.h,
-              ),
-              // Row(
-              //   children: [
-              //     Text(
-              //       "Manzil: ${models.manzil}",
-              //       style: TextStyle(
-              //           color: Colors.black,
-              //           fontFamily: "Inter",
-              //           fontWeight: FontWeight.w400,
-              //           fontSize: 10.sp),
-              //     )
-              //   ],
-              // )
-            ],
-          ),
-        ),
-      ),
-    ),
-  );
-}
+//   return Padding(
+//     padding: EdgeInsets.only(top: 5.h, bottom: 5.0.h, left: 5.w, right: 5.w),
+//     child: InkWell(
+//       onTap: dialog,
+//       child: Container(
+//         width: 353.w,
+//         decoration: BoxDecoration(
+//           borderRadius: BorderRadius.circular(33.w),
+//           border: Border.all(width: 1, color: Colors.black26),
+//           gradient: LinearGradient(
+//               begin: Alignment.topLeft,
+//               end: Alignment.bottomRight,
+//               colors: [colourModels.rang1, colourModels.rang2]),
+//         ),
+//         child: Padding(
+//           padding: const EdgeInsets.all(15.0),
+//           child: Column(
+//             children: [
+//               Row(
+//                 children: [
+//                   Text(
+//                     api.createdBy == null ? "Air ticket" : "${api.createdBy}",
+//                     style: TextStyle(
+//                         color: Colors.black,
+//                         fontFamily: "Inter",
+//                         fontWeight: FontWeight.w400,
+//                         fontSize: 10.sp),
+//                   )
+//                 ],
+//               ),
+//               SizedBox(
+//                 height: 5.h,
+//               ),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.start,
+//                 children: [
+//                   RichText(
+//                       text: TextSpan(children: [
+//                     TextSpan(
+//                       text: "Qayerga: ",
+//                       style: TextStyle(
+//                           color: Colors.black54,
+//                           fontSize: 18.sp,
+//                           fontFamily: "Inter",
+//                           fontWeight: FontWeight.w500),
+//                     ),
+//                     TextSpan(
+//                       text: api.whereTo,
+//                       style: TextStyle(
+//                           color: Colors.black,
+//                           fontSize: 24.sp,
+//                           fontFamily: "Inter",
+//                           fontWeight: FontWeight.w800),
+//                     )
+//                   ])),
+//                 ],
+//               ),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                 children: [
+//                   RichText(
+//                       text: TextSpan(children: [
+//                     TextSpan(
+//                       text: "Qayerdan: ",
+//                       style: TextStyle(
+//                           color: Colors.black54,
+//                           fontSize: 18.sp,
+//                           fontFamily: "Inter",
+//                           fontWeight: FontWeight.w500),
+//                     ),
+//                     TextSpan(
+//                       text: api.fromTo,
+//                       style: TextStyle(
+//                           color: Colors.black,
+//                           fontSize: 24.sp,
+//                           fontFamily: "Inter",
+//                           fontWeight: FontWeight.w800),
+//                     )
+//                   ])),
+//                 ],
+//               ),
+//               SizedBox(
+//                 height: 10.h,
+//               ),
+//               Row(
+//                 children: [Text("Parvoz turi:"), Text(api.airClass!)],
+//               ),
+//               SizedBox(
+//                 height: 10.h,
+//               ),
+//               Row(
+//                 children: [
+//                   Text(
+//                     "Borish kuni: ",
+//                   ),
+//                   Text(DateFormat("dd.MM.yyyy").format(api.toGoDate)),
+//                   SizedBox(
+//                     width: 10.w,
+//                   ),
+//                   Text("Qaytish kuni: "),
+//                   Text(DateFormat("dd.MM.yyyy").format(api.returnDate))
+//                 ],
+//               ),
+//               SizedBox(
+//                 height: 10.h,
+//               ),
+//               SizedBox(
+//                 height: 5.h,
+//               ),
+//               // Row(
+//               //   children: [
+//               //     Text(
+//               //       "Manzil: ${models.manzil}",
+//               //       style: TextStyle(
+//               //           color: Colors.black,
+//               //           fontFamily: "Inter",
+//               //           fontWeight: FontWeight.w400,
+//               //           fontSize: 10.sp),
+//               //     )
+//               //   ],
+//               // )
+//             ],
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
 
 Widget new_design(BuildContext context, Api_models api) {
   return Padding(
@@ -255,7 +249,7 @@ Widget new_design(BuildContext context, Api_models api) {
                                 color: Colors.black,
                                 fontFamily: "Inter",
                                 fontWeight: FontWeight.bold,
-                                fontSize: 24.sp))
+                                fontSize: 16.sp))
                       ],
                     ),
                     Icon(
@@ -279,7 +273,7 @@ Widget new_design(BuildContext context, Api_models api) {
                                 color: Colors.black,
                                 fontFamily: "Inter",
                                 fontWeight: FontWeight.bold,
-                                fontSize: 24.sp))
+                                fontSize: 16.sp))
                       ],
                     ),
                   ],
@@ -328,8 +322,18 @@ Widget new_design(BuildContext context, Api_models api) {
                               fontWeight: FontWeight.bold,
                               fontSize: 14.sp),
                         ),
+                        SizedBox(
+                          height: 5.h,
+                        ),
                         Text(DateFormat("EEEE, dd-MMMM").format(api.toGoDate)),
                       ],
+                    ),
+                    Container(
+                      width: 1.w,
+                      height: 70.h,
+                      decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(100.w)),
                     ),
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.end,
@@ -365,6 +369,9 @@ Widget new_design(BuildContext context, Api_models api) {
                               fontWeight: FontWeight.bold,
                               fontSize: 14.sp),
                         ),
+                        SizedBox(
+                          height: 5.h,
+                        ),
                         Text(
                             DateFormat("EEEE, dd-MMMM").format(api.returnDate)),
                       ],
@@ -374,6 +381,27 @@ Widget new_design(BuildContext context, Api_models api) {
                 const Divider(
                   thickness: 1,
                   color: Colors.grey,
+                ),
+                Row(
+                  children: [
+                    RichText(
+                        text: TextSpan(children: [
+                      TextSpan(
+                          text: "Yo'lovchilar soni: ",
+                          style: TextStyle(
+                              color: Colors.black54,
+                              fontFamily: "Inter",
+                              fontWeight: FontWeight.w600,
+                              fontSize: 14.sp)),
+                      TextSpan(
+                          text: api.passenger.toString(),
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: "Inter",
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14.sp))
+                    ])),
+                  ],
                 ),
               ],
             )),
